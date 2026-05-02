0e64953dc3aad2e4f4db2995f868a85bebd9c7ab
Marianaajpereira <mariana.ajpereira@hotmail.com>
2026-05-02 19:56:22 -0300

    Ajuste trabalho

--- app.py
+++ app.py
@@ -4,11 +4,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return {"status": "ok", "message": "Trabalho Final OpenShift"}
+    return "Hello World"
 
 @app.route('/health')
 def health():
     return {"status": "healthy"}
+
+@app.route('/health/live')
+def health_live():
+    return {"status": "alive"}
+
+@app.route('/health/ready')
+def health_ready():
+    return {"status": "ready"}
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=False)
